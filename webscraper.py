d5595b313167a8efe4d69e85cd98495e8dc68efe
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2022-07-29 23:53:33 -0400

    Add files via upload

---
+++ webscraper.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+'''
+url = 'https://www.youtube.com/c/KalleHallden/videos'
+browser = webdriver.Chrome()
+browser.get(url)
+browser.find_element("xpath", '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-rende'
+                              'rer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-ren'
+                              'derer/div[1]/ytd-grid-video-renderer[1]/div[1]/ytd-thumbnail/a').click()
+'''
+
+url = 'https://www.levels.fyi/leaderboard/Software-Engineer/Software-Engineer/country/United-States/'
+browser = webdriver.Chrome()
+driver.getCurrentUrl();
